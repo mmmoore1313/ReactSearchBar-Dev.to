c0fd367f5da4646e3364a6afe1eb2d8ba90d13cd
Matt Moore <Matt.M.Moore13@outlook.com>
2021-08-13 03:44:48 -0400

    switched the landing page from App.js to App.jsx

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -7,6 +7,23 @@ const useFetch = () => {
     results: [],
   })
   
-  return
+  useEffect(() => {
+    if (data.slug !== "") {
+      const timeoutId = setTimeout(() => {
+        const fetch = async () => {
+          try {
+            const res = await got.get(`/${data.slug}`)
+            setData({ ...data, results: res.data })
+          } catch (err) {
+            console.error(err)
+          }
+        }
+        fetch()
+      }, 1000)
+      return () => clearTimeout(timeoutId)
+    }
+  }, [data.slug])
+  
+  return { data, setData }
 }
 export default useFetch
