86548af476adf27ef89d3367254440f12b9aeb19
Matt Moore <Matt.M.Moore13@outlook.com>
2021-08-13 03:47:10 -0400

    Created a layout for the house members results

---
+++ src/components/Members.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Members({ members }) {
+  return (
+    <ul>
+      {members.map((el, i) => (
+        <li key={i}>
+          {el.name}
+        </li>
+      ))}
+    </ul>
+  )
+}
