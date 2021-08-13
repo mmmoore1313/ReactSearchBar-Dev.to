6386a2ce7e01e65ddaa495063943c946ce776b64
Matt Moore <Matt.M.Moore13@outlook.com>
2021-08-13 02:40:12 -0400

    Created the useFetch.js page to house the searchbar hook component

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react'
+import got from '../api/got'
+
+const useFetch = () => {
+  const [data, setData] = useState({
+    slug: "",
+    results: [],
+  })
+  
+  return
+}
+export default useFetch
