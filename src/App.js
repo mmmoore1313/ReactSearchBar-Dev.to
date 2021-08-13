6c547527a02c290852042174b9af49e6fe2929fc
Matt Moore <Matt.M.Moore13@outlook.com>
2021-08-13 03:43:13 -0400

    switched the landing page from App.js to App.jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+
 import { Router, Link } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <h1>React Search Bar Tutorial</h1>
-      <p>This is an app built to practice creating a functional search bar in React. This was built following the Dev.to article, '<Link to='https://dev.to/franciscomendes10866/how-to-create-a-search-bar-in-react-58nj'>How to Create a Search Bar in React</Link>'</p>
+      <p>This is an app built to practice creating a functional search bar in React. This was built following the Dev.to article, '<Link to='https://dev.to/franciscomendes10866/how-to-create-a-search-bar-in-react-58nj'>How to Create a Search Bar in React</Link>', written by <Link to='https://dev.to/franciscomendes10866'>Francisco Mendes</Link>, <Link to='https://github.com/FranciscoMendes10866'>FranciscoMendes10866</Link></p>
     </div>
   );
 }
